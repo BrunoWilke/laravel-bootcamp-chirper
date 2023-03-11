62362a497b04f99351153611c3012fda2a2d2f70
Bruno Wilke <brunowilke153@gmail.com>
2023-03-11 17:35:36 -0300

    first commit

---
+++ resources/views/chirps/report.blade.php
@@ -0,0 +1,32 @@
+<x-app-layout>
+    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+        <div class="py-2 -my-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+            <div class="inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-lg">
+                <table class="min-w-full">
+                    <thead>
+                        <tr>
+                            <th class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">ID</th>
+                            <th class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">User</th>
+                            <th class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">Message</th>
+                            <th class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">status</th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white">
+                        @foreach ($chirps as $chirp)
+                            <tr>
+                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">{{ $chirp->id }}</td>
+                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                    <div class="text-sm font-medium leading-5 text-gray-900">
+                                        {{ $users[($chirp->user_id) -1]->name }}
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">{{ $chirp->message }}</td>
+                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">{{ $chirp->status }}</td>
+                            </tr>
+                        @endforeach    
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
